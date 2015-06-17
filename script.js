9b4bade174701721ca44ff49c014e7bf3b0afe06
cruzj <you@example.com>
2015-06-17 13:01:21 +0100

    color mixin creator

---
+++ script.js
@@ -0,0 +1,39 @@
+var colorApp = angular.module('colorApp', []);
+colorApp.controller('ColorCtrl', function($scope) {
+  $scope.colors = [
+    {'name': '@color', 'color': '952684;'},
+  ];
+
+  $scope.shadeColor = function(color, percent) {
+    var num = parseInt(color,16),
+        amt = Math.round(2.55 * percent),
+        R = (num >> 16) + amt,
+        G = (num >> 8 & 0x00FF) + amt,
+        B = (num & 0x0000FF) + amt;
+    return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
+  };
+
+  $scope.addName = function() {
+    $scope.colors.length = 1;
+    $scope.colors[0].name = '@color';
+    $scope.colors[0].color = $scope.enteredColor;
+    $scope.posperc = 10;
+    $scope.negperc = -10;
+  };
+
+  $scope.posperc = 10;
+  $scope.negperc = -10;
+
+  $scope.light = function(){
+    
+    var second = {'name': '@color' + '+' + parseInt($scope.posperc), 'color': $scope.shadeColor($scope.colors[$scope.colors.length-1].color, 10)};
+    $scope.colors.push(second);
+    $scope.posperc = $scope.posperc + 10;
+  };
+
+  $scope.dark = function(){
+    var second = {'name': '@color' + parseInt($scope.negperc) , 'color': $scope.shadeColor($scope.colors[0].color, -10)};
+    $scope.colors.unshift(second);
+    $scope.negperc -= 10;
+  };
+});
