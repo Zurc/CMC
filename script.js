756b64d8ec8dbe8ba3c7e588e7742afcb0ffb3f8
cruzj <you@example.com>
2015-06-18 11:15:04 +0100

    cmyk colors added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var colorApp = angular.module('colorApp', []);
 colorApp.controller('ColorCtrl', function($scope) {
   $scope.colors = [
-    {'name': '@color', 'hexcolor': '952684;', 'rgbcolor': 'rgb(149,38,132)'},
+    {
+      'name': '@color', 
+      'hexcolor': '952684;', 
+      'rgbcolor': 'rgb(149,38,132)',
+      'cmykcolor': 'cmyk(0,74,11,42)'
+    },
   ];
 
   // to change hex color in a certain percentage
@@ -24,11 +29,45 @@ colorApp.controller('ColorCtrl', function($scope) {
     return "rgb(" + r + "," + g + "," + b + ")";
   };
 
+  //  to transform hex to cmyk color
+  $scope.hexToCMYK = function(hexcolor) {
+   var computedC = 0;
+   var computedM = 0;
+   var computedY = 0;
+   var computedK = 0;
+
+   hexcolor = (hexcolor.charAt(0)=="#") ? hexcolor.substring(1,7) : hexcolor;
+
+   var r = parseInt(hexcolor.substring(0,2),16); 
+   var g = parseInt(hexcolor.substring(2,4),16); 
+   var b = parseInt(hexcolor.substring(4,6),16); 
+
+   // BLACK
+   if (r==0 && g==0 && b==0) {
+    computedK = 1;
+    return "cmyk(0,0,0,1)";
+   }
+
+   computedC = 1 - (r/255);
+   computedM = 1 - (g/255);
+   computedY = 1 - (b/255);
+
+   var minCMY = Math.min(computedC,Math.min(computedM,computedY));
+
+   computedC = Math.round((computedC - minCMY) / (1 - minCMY) * 100) ;
+   computedM = Math.round((computedM - minCMY) / (1 - minCMY) * 100) ;
+   computedY = Math.round((computedY - minCMY) / (1 - minCMY) * 100) ;
+   computedK = Math.round(minCMY * 100);
+
+   return "cmyk(" + computedC + "," + computedM + "," + computedY + "," + computedK + ")";
+  }
+
   $scope.addColor = function() {
     $scope.colors.length = 1;
     $scope.colors[0].name = '@color';
     $scope.colors[0].hexcolor = $scope.enteredColor;
     $scope.colors[0].rgbcolor = $scope.hexToRgb($scope.colors[0].hexcolor);
+    $scope.colors[0].cmykcolor = $scope.hexToCMYK($scope.colors[0].hexcolor);
     $scope.posperc = 10;
     $scope.negperc = -10;
   };
@@ -38,18 +77,22 @@ colorApp.controller('ColorCtrl', function($scope) {
 
   $scope.light = function(){
     
-    var next = {'name': '@color' + '+' + parseInt($scope.posperc), 
+    var next = {
+                'name': '@color' + '+' + parseInt($scope.posperc), 
                 'hexcolor': $scope.shadeHexColor($scope.colors[$scope.colors.length-1].hexcolor, 10), 
-                'rgbcolor': $scope.hexToRgb($scope.shadeHexColor($scope.colors[$scope.colors.length-1].hexcolor, 10))
+                'rgbcolor': $scope.hexToRgb($scope.shadeHexColor($scope.colors[$scope.colors.length-1].hexcolor, 10)),
+                'cmykcolor': $scope.hexToCMYK($scope.shadeHexColor($scope.colors[$scope.colors.length-1].hexcolor, 10))
               };
     $scope.colors.push(next);
     $scope.posperc = $scope.posperc + 10;
   };
 
   $scope.dark = function(){
-    var previous = {'name': '@color' + parseInt($scope.negperc) , 
+    var previous = {
+                'name': '@color' + parseInt($scope.negperc) , 
                 'hexcolor': $scope.shadeHexColor($scope.colors[0].hexcolor, -10),
-                'rgbcolor': $scope.hexToRgb($scope.shadeHexColor($scope.colors[0].hexcolor, -10))
+                'rgbcolor': $scope.hexToRgb($scope.shadeHexColor($scope.colors[0].hexcolor, -10)),
+                'cmykcolor': $scope.hexToCMYK($scope.shadeHexColor($scope.colors[0].hexcolor, -10))
               };
     $scope.colors.unshift(previous);
     $scope.negperc -= 10;
